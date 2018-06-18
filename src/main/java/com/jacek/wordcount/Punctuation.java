314771ce2d6851c91ba8142a2abc28bd1615103b
Jacek R. Ambroziak <jacek.ambroziak@gmail.com>
2018-06-18 11:32:28 -0400

    added Punctuation stripping test

--- src/main/java/com/jacek/wordcount/Punctuation.java
+++ src/main/java/com/jacek/wordcount/Punctuation.java
@@ -1,7 +1,5 @@
 package com.jacek.wordcount;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 /**
  * Utility class defining a set punctuation characters
  * and providing a method to strip leading and trailing punctuation characters from input String
@@ -23,24 +21,28 @@ final class Punctuation {
     }
 
     /**
-     * @param input non-null string
+     * @param input string possibly starting/ending with punctuation
      * @return substring of input with leading and trailing punctuation characters removed
      */
-    static String stripPunctuation(@NonNull final String input) {
-        int j = input.length();
-        // look for trailing punctuation chars in input
-        while (--j >= 0 && isPunctuation(input.charAt(j))) {
-            // continue
-        }
-        if (j < 0) {    // string was empty or contained only punctuation
-            return EMPTY_STRING;
-        } else {    // j >= 0, points at some non-punctuation char
-            int i = 0;
-            // look for leading punctuation characters
-            while (i < j && isPunctuation(input.charAt(i))) {
-                ++i;
+    static String stripPunctuation(final String input) {
+        if (input != null) {
+            int j = input.length();
+            // look for trailing punctuation chars in input
+            while (--j >= 0 && isPunctuation(input.charAt(j))) {
+                // continue
             }
-            return input.substring(i, j + 1);
+            if (j < 0) {    // string was empty or contained only punctuation
+                return EMPTY_STRING;
+            } else {    // j >= 0, points at some non-punctuation char
+                int i = 0;
+                // look for leading punctuation characters
+                while (i < j && isPunctuation(input.charAt(i))) {
+                    ++i;
+                }
+                return input.substring(i, j + 1);
+            }
+        } else {
+            return null;
         }
     }
 }
--- src/test/java/com/jacek/wordcount/PunctuationTest.java
+++ src/test/java/com/jacek/wordcount/PunctuationTest.java
@@ -1,10 +1,29 @@
 package com.jacek.wordcount;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public final class PunctuationTest {
     @Test
     public void test() {
-
+        Assert.assertEquals(Punctuation.stripPunctuation(null), null);
+        Assert.assertEquals(Punctuation.stripPunctuation(""), "");
+        Assert.assertEquals(Punctuation.stripPunctuation("abc"), "abc");
+        Assert.assertEquals(Punctuation.stripPunctuation(",.!"), "");
+        // complete set
+        Assert.assertEquals(Punctuation.stripPunctuation("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"), "");
+        Assert.assertEquals(Punctuation.stripPunctuation("/abc"), "abc");
+        Assert.assertEquals(Punctuation.stripPunctuation("abc/"), "abc");
+        Assert.assertEquals(Punctuation.stripPunctuation("/abc/"), "abc");
+        // inside punctuation not stripped
+        Assert.assertEquals(Punctuation.stripPunctuation("/ab/c/"), "ab/c");
+        Assert.assertEquals(Punctuation.stripPunctuation("a+b-c"), "a+b-c");
+        // space IS NOT punctuation!
+        Assert.assertEquals(Punctuation.stripPunctuation(" abc "), " abc ");
+        Assert.assertEquals(Punctuation.stripPunctuation(" ab\tc "), " ab\tc ");
+        // multiple leading and trailing punctuation + embedded
+        Assert.assertEquals(Punctuation.stripPunctuation("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" +
+                "AB/C" +
+                "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"), "AB/C");
     }
 }
